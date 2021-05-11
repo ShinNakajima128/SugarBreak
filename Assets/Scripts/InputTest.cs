9f48b8324c6d5be6b01aa844c33ca6103a0b7e8d
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-05-11 22:55:48 +0900

    InputSystemを導入しTestした。

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour, ExampleControls.IPlayerActions
+{
+    ExampleControls m_input;
+
+    void Awake()
+    {
+        m_input = new ExampleControls();
+
+        m_input.Player.SetCallbacks(this);
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Fire");
+        }
+    }
+
+    void OnEnable()
+    {
+        m_input.Enable();
+    }
+
+    void OnDisable()
+    {
+        m_input.Disable();
+    }
+
+    void OnDestroy()
+    {
+        m_input.Dispose();
+    }
+}
