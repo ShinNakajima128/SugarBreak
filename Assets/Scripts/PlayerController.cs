d1fbf19097682b40e6fe29c48822ab797623d5d0
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-05-30 18:37:09 +0900

    ステージ、プレイヤー、オブジェクト等をTest中のSceneに配置した。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>動く速さ</summary>
+    [SerializeField] float m_movingSpeed = 5f;
+    /// <summary>ターンの速さ</summary>
+    [SerializeField] float m_turnSpeed = 3f;
+    /// <summary>ジャンプ力</summary>
+    [SerializeField] float m_jumpPower = 5f;
+    /// <summary> プレイヤーが操作可能か否か </summary>
+    public bool m_playerOperation = true;
+    /// <summary> プレイヤーのRigidbody </summary>
+    Rigidbody m_rb;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    bool IsGrounded()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 1f);
+        bool isGrounded = hitColliders.Length > 0;
+        return isGrounded;
+    }
+
+    void PlayerMove()
+    {
+        // 方向の入力を取得し、方向を求める
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // 入力方向のベクトルを組み立てる
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+
+        if (dir == Vector3.zero)
+        {
+            // 方向の入力がニュートラルの時は、y 軸方向の速度を保持するだけ
+            m_rb.velocity = new Vector3(0f, m_rb.velocity.y, 0f);
+        }
+        else
+        {
+            // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+            dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+            dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+
+            // 入力方向に滑らかに回転させる
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);  // Slerp を使うのがポイント
+
+            Vector3 velo = dir.normalized * m_movingSpeed; // 入力した方向に移動する
+            velo.y = m_rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
+            m_rb.velocity = velo;   // 計算した速度ベクトルをセットする
+        }
+    }
+}
