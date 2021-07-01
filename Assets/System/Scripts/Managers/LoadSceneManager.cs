59adcef2b8a5f137407bacf806f57a57a1e6c97a
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-02 03:07:45 +0900

    Fade-in, Fade-outのコンポーネントを追加した。

---
+++ Assets/System/Scripts/Managers/LoadSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneManager : MonoBehaviour
+{
+    [SerializeField] Fade fade = null;
+
+    void Start()
+    {
+        fade.FadeOut(2.0f, () =>
+        {
+            Debug.Log("フェード開始");
+        });
+    }
+}
