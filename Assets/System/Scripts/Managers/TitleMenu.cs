14f61d9596d6719c773352a6808cae63c8cc91d9
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-10 20:34:52 +0900

    TitleMenuの機能を作成し始めた。

---
+++ Assets/System/Scripts/Managers/TitleMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TitleMenuState
+{
+    Begin,
+    MainMenu,
+    Option,
+    Audio
+}
+
+public class TitleMenu : MonoBehaviour
+{
+    [SerializeField] GameObject m_titleMenuPanel = default;
+    [SerializeField] Fade fade = default;
+    [SerializeField] FadeImage fadeImage = default;
+    [SerializeField] SoundManager soundManager = default;
+    [SerializeField] float m_loadTime = 1.0f;
+    [SerializeField] Texture m_masks = default;
+    [SerializeField] TitleMenuState titleState = TitleMenuState.Begin;
+    static bool isStarted = false;
+    bool isChanged = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                
+            }
+        }
+
+        switch (titleState)
+        {
+            case TitleMenuState.Begin:
+                if (!isChanged)
+                {
+                    m_titleMenuPanel.SetActive(true);
+                }
+                break;
+        }
+    }
+
+    IEnumerator StartWait()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+
+    }
+}
