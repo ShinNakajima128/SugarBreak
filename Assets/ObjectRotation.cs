a08440dbadd4b8af02a3b1d1621156386cd22e06
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-16 16:24:57 +0900

    タイトル画面の雲がゆったり流れていくようにした。

---
+++ Assets/ObjectRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    [SerializeField] float m_rotateSpeed = default;
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, m_rotateSpeed, 0));
+    }
+}
