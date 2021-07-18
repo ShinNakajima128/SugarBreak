8c78309df538a311bd339b2e235a8e3eb07d4ebb
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-07-18 23:22:57 +0900

    Skyboxを回転させるようにした

---
+++ Assets/System/Scripts/SkyboxController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    [SerializeField] float anglePerFrame = 0.1f;
+    float rot = 0.0f;
+    float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 0.1f)
+        {
+            rot += anglePerFrame;
+            if (rot >= 360.0f)
+            {
+                rot -= 360.0f;
+            }
+            RenderSettings.skybox.SetFloat("_Rotation", rot);
+            timer = 0;
+        }
+    }
+}
