baf026aac93e71ad9f2727a0d1e052ef5e434564
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-26 16:25:52 +0900

    SoundManagerを修正した

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -3,29 +3,35 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public enum BGMType
-{
-
-}
-
-public enum SEType
-{
-
-}
 
 public class SoundManager : SingletonMonoBehaviour<SoundManager>
 {
-    public static float m_masterVolume = 1.0f;
-    public static float m_bgmVolume = 0.1f;
-    public static float m_seVolume = 1.0f;
+    [SerializeField, Range(0f, 1f)] float m_masterVolume = 1.0f;
+    [SerializeField, Range(0f, 1f)] float m_bgmVolume = 0.1f;
+    [SerializeField, Range(0f, 1f)] float m_seVolume = 1.0f;
+    [SerializeField, Range(0f, 1f)] float m_voiceVolume = 1.0f;
     [SerializeField] AudioClip[] m_bgms = null;
     [SerializeField] AudioClip[] m_ses = null;
+    [SerializeField] AudioClip[] m_voices = null;
     [SerializeField] AudioSource m_bgmAudioSource = null;
     [SerializeField] AudioSource m_seAudioSource = null;
+    [SerializeField] AudioSource m_voiceAudioSource = null;
     Dictionary<string, int> bgmIndex = new Dictionary<string, int>();
     Dictionary<string, int> seIndex = new Dictionary<string, int>();
-    public static bool isLosted = false;
+    Dictionary<string, int> voiceIndex = new Dictionary<string, int>();
+    /// <summary> マスター音量時のフラグ </summary>
+    bool masterVolumeChange = false;
+    /// <summary> BGM音量時のフラグ </summary>
+    bool bgmVolumeChange = false;
+    /// <summary> SE音量時のフラグ </summary>
+    bool seVolumeChange = false;
+    /// <summary> ボイス音量時のフラグ </summary>
+    bool voiceVolumeChange = false;
 
+    public float GetMasterVolume { get => m_masterVolume; }
+    public float GetBgmVolume { get => m_bgmVolume; }
+    public float GetSeVolume { get => m_seVolume; }
+    public float GetVoiceVolume { get => m_voiceVolume; }
 
     void Awake()
     {
@@ -111,8 +117,73 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
 
     void Update()
     {
-        m_bgmAudioSource.volume = m_bgmVolume * m_masterVolume;
-        m_seAudioSource.volume = m_seVolume * m_masterVolume;
+        VolumeChanger();
+    }
+
+    /// <summary>
+    /// 各音量を変更する
+    /// </summary>
+    public void VolumeChanger()
+    {
+        if (m_bgmAudioSource && bgmVolumeChange || m_bgmAudioSource && masterVolumeChange)
+        {
+            m_bgmAudioSource.volume = m_bgmVolume * m_masterVolume;
+            if (masterVolumeChange) masterVolumeChange = false;
+            if (bgmVolumeChange) bgmVolumeChange = false;
+        }
+
+        if (m_seAudioSource && seVolumeChange || m_seAudioSource && masterVolumeChange)
+        {
+            m_seAudioSource.volume = m_seVolume * m_masterVolume;
+            if (masterVolumeChange) masterVolumeChange = false;
+            if (seVolumeChange) seVolumeChange = false;
+        }
+
+        if (m_voiceAudioSource && voiceVolumeChange || m_voiceAudioSource && masterVolumeChange)
+        {
+            m_voiceAudioSource.volume = m_voiceVolume * m_masterVolume;
+            if (masterVolumeChange) masterVolumeChange = false;
+            if (voiceVolumeChange) voiceVolumeChange = false;
+        }
+    }
+    /// <summary>
+    /// マスター音量を変更する
+    /// </summary>
+    /// <param name="masterValue"> 音量 </param>
+    public void MasterVolChange(float masterValue)
+    {
+        m_masterVolume = masterValue;
+        masterVolumeChange = true;
+    }
+
+    /// <summary>
+    /// BGM音量を変更する
+    /// </summary>
+    /// <param name="bgmValue"> 音量 </param>
+    public void BgmVolChange(float bgmValue)
+    {
+        m_bgmVolume = bgmValue;
+        bgmVolumeChange = true;
+    }
+
+    /// <summary>
+    /// SE音量を変更する
+    /// </summary>
+    /// <param name="seValue"> 音量 </param>
+    public void SeVolChange(float seValue)
+    {
+        m_seVolume = seValue;
+        seVolumeChange = true;
+    }
+
+    /// <summary>
+    /// ボイス音量を変更する
+    /// </summary>
+    /// <param name="voiceValue"> 音量 </param>
+    public void VoiceVolChange(float voiceValue)
+    {
+        m_voiceVolume = voiceValue;
+        voiceVolumeChange = true;
     }
 
     int GetBgmIndex(string name)
@@ -138,7 +209,7 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
             return 0;
         }
     }
-    public void PlayBgm(int index)
+    void PlayBgm(int index)
     {
         if (Instance != null)
         {
@@ -179,21 +250,5 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
     {
         m_seAudioSource.Stop();
         m_seAudioSource.clip = null;
-    }
-
-    public void MasterVolChange()
-    {
-        m_masterVolume = GameObject.FindGameObjectWithTag("Master").GetComponent<Slider>().value;
-        Debug.Log(m_masterVolume);
-    }
-    public void BGMVolChange()
-    {
-        m_bgmVolume = GameObject.FindGameObjectWithTag("BGM").GetComponent<Slider>().value;
-        Debug.Log(m_bgmVolume);
-    }
-    public void SEVolChange()
-    {
-        m_seVolume = GameObject.FindGameObjectWithTag("SE").GetComponent<Slider>().value;
-        Debug.Log(m_seVolume);
     }
 }
