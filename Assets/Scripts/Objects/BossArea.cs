0b4eeb1e7123f01f3c81e22a2163d60096ef51b8
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-31 20:10:25 +0900

    ボスエリアを作成した

---
+++ Assets/Scripts/Objects/BossArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SoundManager.Instance.PlayBgmByName("BossBattle");
+            Debug.Log("ボスエリアに入った");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (SceneManager.GetActiveScene().name == "BakedValley")
+            {
+                SoundManager.Instance.PlayBgmByName("BakedValley");
+                Debug.Log("ボスエリアを抜けた");
+            }
+        }
+    }
+}
