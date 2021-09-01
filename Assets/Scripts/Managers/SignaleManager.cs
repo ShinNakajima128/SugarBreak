4f6e3e75d69081edde4ced460ea338d12d1c0c05
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-09-01 19:30:29 +0900

    ゲーム開始時にムービーが流れるようにした

---
+++ Assets/Scripts/Managers/SignaleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignaleManager : MonoBehaviour
+{
+    [SerializeField] Fade fade = default;
+
+    public void FadeIn()
+    {
+        LoadSceneManager.Instance.FadeIn();
+    }
+
+    public void FadeOut()
+    {
+        LoadSceneManager.Instance.FadeOut();
+    }
+}
---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_stageName = default;
+
+    public void EnableStageName()
+    {
+        m_stageName.SetActive(true);
+    }
+    public void DisableStageName()
+    {
+        m_stageName.SetActive(false);
+    }
+}
