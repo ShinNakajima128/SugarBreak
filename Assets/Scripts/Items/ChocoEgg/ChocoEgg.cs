7093f5ad68e60bc58d8778365e22176047a331e7
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-09-07 20:19:35 +0900

    ボスからドロップするチョコエッグを仮で作成した

---
+++ Assets/Scripts/Items/ChocoEgg/ChocoEgg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChocoEgg : MonoBehaviour, IDamagable
+{
+    [SerializeField]
+    GameObject[] m_dropItems = default;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int attackPower)
+    {
+        throw new System.NotImplementedException();
+    }
+}
