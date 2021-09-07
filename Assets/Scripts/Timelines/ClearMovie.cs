329c12239330f31999be2d90a4bff9de4f1a61d1
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-09-08 01:57:43 +0900

    ステージクリア時の演出を再生する用のスクリプトを作成した

---
+++ Assets/Scripts/Timelines/ClearMovie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ClearMovie : MonoBehaviour
+{
+    PlayableDirector director;
+
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+        GameManager.GameEnd += PlayMovie;
+    }
+
+    void PlayMovie()
+    {
+        Debug.Log("クリア演出再生");
+        //director.Play();
+    }
+}
