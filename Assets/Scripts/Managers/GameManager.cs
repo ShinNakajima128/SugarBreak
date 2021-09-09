7e02a10753fb702b9af2c1a5f7a54fe71b067303
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-09-09 13:39:12 +0900

    ゲームマネージャーにステージ情報が更新された時などのフラグを追加した

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
+/// <summary>
+/// ゲーム全体を管理するクラス
+/// </summary>
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
+    /// <summary>
+    /// ステージクリア時のイベント
+    /// </summary>
     public static Action GameEnd;
+
+    /// <summary>
+    /// ステージの更新がある時のフラグ
+    /// </summary>
+    public bool IsStageUpdated { get; set; } = false;
+
+    /// <summary>
+    /// ベイクルバレーのクリアフラグ
+    /// </summary>
+    public bool IsBakeleValleyCleared { get; set; } = false;
 
     void Awake()
     {
