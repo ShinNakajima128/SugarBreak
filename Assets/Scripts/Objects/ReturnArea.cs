3f7ca1b741ead0818fc80df28f9ea77aa66e0a43
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-09-11 15:58:28 +0900

    ステージから落下したらスタート地点に戻るようにした

---
+++ Assets/Scripts/Objects/ReturnArea.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnArea : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_returnPoint = default;
+
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerStatesManager.Instance.OffOperation();
+            LoadSceneManager.Instance.FadeIn();
+            StartCoroutine(Return());
+        }
+    }
+
+    IEnumerator Return()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        player.transform.position = m_returnPoint.transform.position;
+
+        yield return new WaitForSeconds(1.0f);
+
+        LoadSceneManager.Instance.FadeOut();
+        PlayerStatesManager.Instance.OnOperation();
+    }
+}
