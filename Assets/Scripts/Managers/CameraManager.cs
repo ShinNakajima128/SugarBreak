bfe1f129e29332ba3372aa549cce30f7e425e835
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-09-11 20:33:34 +0900

    カメラの位置をリセットさせる機能を作成した

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance;
+
+    [SerializeField]
+    Transform m_player = default;
+
+    [SerializeField]
+    CinemachineFreeLook m_freeLook = default;
+
+    Vector3 m_initialPos;
+
+    private void Start()
+    {
+        Instance = this;
+        m_initialPos = m_freeLook.transform.localPosition;
+
+        Debug.Log(m_initialPos);
+    }
+
+    public void CameraReset()
+    {
+        StartCoroutine(ResetPosition());
+    }
+
+    IEnumerator ResetPosition()
+    {
+        m_freeLook.Follow = null;
+        Debug.Log("リセット開始");
+        yield return null;
+        m_freeLook.transform.localPosition = m_initialPos;
+        Debug.Log("初期位置に戻す");
+        yield return null;
+        m_freeLook.Follow = m_player;
+        Debug.Log("リセット完了");
+    }
+}
--- Assets/Scripts/Objects/ReturnAreas/ReturnArea.cs
+++ Assets/Scripts/Objects/ReturnAreas/ReturnArea.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 触れたら現在の復帰場所に画面を暗転しながら移動させるクラス
+/// </summary>
 public class ReturnArea : MonoBehaviour
 {
     public static ReturnArea Instance;
@@ -9,6 +12,8 @@ public class ReturnArea : MonoBehaviour
     GameObject player;
 
     public Vector3 ReturnPoint { get; set; }
+
+    public Quaternion ReturnRotation { get; set; }
 
     private void Awake()
     {
@@ -18,6 +23,8 @@ public class ReturnArea : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        ReturnPoint = player.transform.position;
+        ReturnRotation = player.transform.rotation;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -35,10 +42,11 @@ public class ReturnArea : MonoBehaviour
         yield return new WaitForSeconds(1.0f);
 
         player.transform.position = ReturnPoint;
+        player.transform.rotation = ReturnRotation;
 
         yield return new WaitForSeconds(1.0f);
-
         LoadSceneManager.Instance.FadeOut();
         PlayerStatesManager.Instance.OnOperation();
+        CameraManager.Instance.CameraReset();
     }
 }
