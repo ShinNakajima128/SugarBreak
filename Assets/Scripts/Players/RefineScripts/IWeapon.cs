03cff44b67f2c2bef67304db404e29be0d27bbf1
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-01 22:13:54 +0900

    武器の機能のインターフェースを修正

--- Assets/Scripts/Players/RefineScripts/IWeapon.cs
+++ Assets/Scripts/Players/RefineScripts/IWeapon.cs
@@ -1,7 +1,8 @@
-﻿
+﻿using UnityEngine;
+
 public interface IWeapon
 {
-    void WeaponAttack();
-    void BeginAttack();
-    void EndAttack();
+    void WeaponAction1(Animator anim, Rigidbody rb, int comboNum = 0);
+    void WeaponAction2(Animator anim, Rigidbody rb);
+    void WeaponAction3(Animator anim, Rigidbody rb);
 }
