8eaca3e4f2248928d42ccb4196c405490d7ea62a
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-02 12:32:08 +0900

    各武器のエフェクトを再生するための機能を実装

--- Assets/Scripts/Weapons/WeaponActionManager.cs
+++ Assets/Scripts/Weapons/WeaponActionManager.cs
@@ -8,9 +8,18 @@ using UnityEngine.Events;
 /// </summary>
 public enum ActionType
 {
+    /// <summary> 攻撃1 </summary>
     Action1,
+    /// <summary> 攻撃2 </summary>
     Action2,
-    Action3
+    /// <summary> 攻撃3 </summary>
+    Action3,
+    /// <summary> 当たり判定開始 </summary>
+    StartHitDecision,
+    /// <summary> 当たり判定終了 </summary>
+    FinishHitDecision,
+    /// <summary> 各武器のエフェクトを再生 </summary>
+    WeaponEffect
 }
 
 /// <summary>
@@ -18,11 +27,18 @@ public enum ActionType
 /// </summary>
 public class WeaponActionManager : MonoBehaviour
 {
+    /// <summary> 攻撃1の機能を持つUnityEvent </summary>
     UnityEvent m_weaponAction1 = new UnityEvent();
-
+    /// <summary> 攻撃2の機能を持つUnityEvent </summary>
     UnityEvent m_weaponAction2 = new UnityEvent();
-
+    /// <summary> 攻撃3の機能を持つUnityEvent </summary>
     UnityEvent m_weaponAction3 = new UnityEvent();
+    /// <summary> 当たり判定開始の機能を持つUnityEvent </summary>
+    UnityEvent m_startHitDecision = new UnityEvent();
+    /// <summary> 当たり判定終了の機能を持つUnityEvent </summary>
+    UnityEvent m_finishHitDecision = new UnityEvent();
+    /// <summary> 各武器のエフェクト再生の機能を持つUnityEvent </summary>
+    UnityEvent m_weaponEffect = new UnityEvent();
 
     static WeaponActionManager instance;
 
@@ -56,6 +72,27 @@ public class WeaponActionManager : MonoBehaviour
     }
 
     /// <summary>
+    /// 近接武器の当たり判定の受付を開始する
+    /// </summary>
+    public void OnStartHitDesition()
+    {
+        m_startHitDecision?.Invoke();
+    }
+
+    /// <summary>
+    /// 近接武器の当たり判定の受付を終了する
+    /// </summary>
+    public void OnFinishHitDesition()
+    {
+        m_finishHitDecision?.Invoke();
+    }
+
+    public void OnWeaponEffect()
+    {
+        m_weaponEffect?.Invoke();
+    }
+
+    /// <summary>
     /// 武器の機能を登録する
     /// </summary>
     /// <param name="type"> アクションの種類 </param>
@@ -72,6 +109,15 @@ public class WeaponActionManager : MonoBehaviour
                 break;
             case ActionType.Action3:
                 instance.m_weaponAction3.AddListener(action);
+                break;
+            case ActionType.StartHitDecision:
+                instance.m_startHitDecision.AddListener(action);
+                break;
+            case ActionType.FinishHitDecision:
+                instance.m_finishHitDecision.AddListener(action);
+                break;
+            case ActionType.WeaponEffect:
+                instance.m_weaponEffect.AddListener(action);
                 break;
             default:
                 Debug.LogError("値が不正なため、コードの記述ミスの可能性有");
@@ -96,6 +142,15 @@ public class WeaponActionManager : MonoBehaviour
                 break;
             case ActionType.Action3:
                 instance.m_weaponAction3.RemoveListener(action);
+                break;
+            case ActionType.StartHitDecision:
+                instance.m_startHitDecision.RemoveListener(action);
+                break;
+            case ActionType.FinishHitDecision:
+                instance.m_finishHitDecision.RemoveListener(action);
+                break;
+            case ActionType.WeaponEffect:
+                instance.m_weaponEffect.RemoveListener(action);
                 break;
             default:
                 Debug.LogError("値が不正なため、コードの記述ミスの可能性有");
