e064174fa3309d87c57fbbaaeb947a5140d1ed58
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-08 22:05:46 +0900

    着弾地点のスクロール速度を変更できるように修正

--- Assets/Scripts/Players/AimRotation.cs
+++ Assets/Scripts/Players/AimRotation.cs
@@ -11,17 +11,22 @@ public class AimRotation : MonoBehaviour
     [SerializeField]
     float m_rotateSpeed = 3.0f;
 
+    [Header("スクロールスピード")]
+    [SerializeField]
+    float m_scrollSpeed = 6.0f;
+
     [Header("上方向の上限")]
     [SerializeField]
-    float m_maxAngle = 5;
+    float m_maxValue = 30;
 
     [Header("下方向の下限")]
     [SerializeField]
-    float m_minAngle = -5;
+    float m_minValue = -30;
 
     Transform m_playerTrans = default;
     Transform m_weaponListTrans = default;
     Quaternion m_weaponListOriginTrans;
+    float m_currentValue = 0;
     public static AimRotation Instance { get; private set; }
 
     void Awake()
@@ -49,9 +54,25 @@ public class AimRotation : MonoBehaviour
         //プレイヤーの上下角度が上限に達していたらそれ以上は向かないようにする
         
         Vector3 playerAngle = new Vector3(0, h * m_rotateSpeed, 0);
-        Vector3 weaponListAngle = new Vector3(scroll * m_rotateSpeed * 2, 0, 0);
+        Vector3 weaponListAngle = new Vector3(scroll * m_scrollSpeed, 0, 0);
+        m_currentValue += weaponListAngle.x;
+        Debug.Log(m_currentValue);
         m_playerTrans.Rotate(playerAngle);
-        m_weaponListTrans.Rotate(weaponListAngle);
+
+        if (m_currentValue >= m_maxValue)
+        {
+            m_currentValue = m_maxValue;
+            return;
+        }
+        else if (m_currentValue <= m_minValue)
+        {
+            m_currentValue = m_minValue;
+            return;
+        }
+        else
+        {
+            m_weaponListTrans.Rotate(weaponListAngle);
+        }
     } 
 
     /// <summary>
