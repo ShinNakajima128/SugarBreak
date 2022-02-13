ab9f051eb9d4d79e856f82246a9b239550b047b1
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-14 06:23:19 +0900

    Animationフォルダを整頓

---
+++ Assets/Scripts/Enemys/BossMotionTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BossMotionTest : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_walkEffectPos = default;
+
+    [SerializeField]
+    Transform m_attackEffectPos = default;
+
+    public void Walk()
+    {
+        EventManager.OnEvent(Events.CameraShake); //カメラを揺らす
+        SoundManager.Instance.PlaySeByName("怪獣の足音");
+        EffectManager.PlayEffect(EffectType.Explosion, m_walkEffectPos.position);
+    }
+    public void Attack()
+    {
+        EventManager.OnEvent(Events.CameraShake); //カメラを揺らす
+        SoundManager.Instance.PlaySeByName("全力で踏み込む");
+        EffectManager.PlayEffect(EffectType.Landing, m_attackEffectPos.position);
+    }
+}
