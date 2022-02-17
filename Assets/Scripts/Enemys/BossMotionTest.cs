92eea729a3354999f740d7f8426b50195afb9a27
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-17 23:44:16 +0900

    ボスの攻撃の当たり判定の機能を追加

--- Assets/Scripts/Enemys/BossMotionTest.cs
+++ Assets/Scripts/Enemys/BossMotionTest.cs
@@ -6,10 +6,19 @@ using Cinemachine;
 public class BossMotionTest : MonoBehaviour
 {
     [SerializeField]
+    int m_attackPower = 2;
+    
+    [SerializeField]
     Transform m_walkEffectPos = default;
 
     [SerializeField]
     Transform m_attackEffectPos = default;
+
+    [SerializeField]
+    BoxCollider m_attackCollider = default;
+
+    [SerializeField]
+    HitDecision m_hd = default;
 
     public void Walk()
     {
@@ -17,10 +26,22 @@ public class BossMotionTest : MonoBehaviour
         SoundManager.Instance.PlaySeByName("怪獣の足音");
         EffectManager.PlayEffect(EffectType.Explosion, m_walkEffectPos.position);
     }
+
     public void Attack()
     {
         EventManager.OnEvent(Events.CameraShake); //カメラを揺らす
         SoundManager.Instance.PlaySeByName("全力で踏み込む");
         EffectManager.PlayEffect(EffectType.Landing, m_attackEffectPos.position);
+        m_hd.AttackDamage = m_attackPower;
+    }
+
+    public void OnAttack1Collider()
+    {
+        m_attackCollider.enabled = true;
+    }
+
+    public void OffAttack1Collider()
+    {
+        m_attackCollider.enabled = false;
     }
 }
---
+++ Assets/Scripts/Enemys/HitDecision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 攻撃判定を行うクラス
+/// </summary>
+public class HitDecision : MonoBehaviour
+{
+    public int AttackDamage { get; set; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        var target = other.GetComponent<IDamagable>();
+        if (target != null)
+        {
+            target.Damage(AttackDamage);
+        }
+    }
+}
