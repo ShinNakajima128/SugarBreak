274cc2478e764c486243de74b4db4a69ae804296
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-19 22:47:43 +0900

    コメントを追加

--- Assets/Scripts/Enemys/BossMotionTest.cs
+++ Assets/Scripts/Enemys/BossMotionTest.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
+/// <summary>
+/// ボスの機能のテスト用クラス
+/// </summary>
 public class BossMotionTest : MonoBehaviour
 {
+    /// <summary> 攻撃力 </summary>
     [SerializeField]
     int m_attackPower = 2;
-    
+
+    /// <summary> 歩行時のエフェクトを出す位置 </summary>
     [SerializeField]
     Transform m_walkEffectPos = default;
 
+    /// <summary> 攻撃のエフェクトを出す位置 </summary>
     [SerializeField]
     Transform m_attackEffectPos = default;
 
+    /// <summary> 攻撃の当たり判定用のCollider </summary>
     [SerializeField]
     BoxCollider m_attackCollider = default;
 
+    /// <summary> Playerにヒットしたか判定するクラス </summary>
     [SerializeField]
     HitDecision m_hd = default;
 
+    /// <summary>
+    /// 歩く
+    /// </summary>
     public void Walk()
     {
         EventManager.OnEvent(Events.CameraShake); //カメラを揺らす
@@ -27,7 +38,10 @@ public class BossMotionTest : MonoBehaviour
         EffectManager.PlayEffect(EffectType.Explosion, m_walkEffectPos.position);
     }
 
-    public void Attack()
+    /// <summary>
+    /// 攻撃1
+    /// </summary>
+    public void Attack1()
     {
         EventManager.OnEvent(Events.CameraShake); //カメラを揺らす
         SoundManager.Instance.PlaySeByName("全力で踏み込む");
@@ -35,11 +49,16 @@ public class BossMotionTest : MonoBehaviour
         m_hd.AttackDamage = m_attackPower;
     }
 
+    /// <summary>
+    /// 攻撃1の当たり判定をONにする
+    /// </summary>
     public void OnAttack1Collider()
     {
         m_attackCollider.enabled = true;
     }
-
+    /// <summary>
+    /// 攻撃1の当たり判定をOFFにする
+    /// </summary>
     public void OffAttack1Collider()
     {
         m_attackCollider.enabled = false;
