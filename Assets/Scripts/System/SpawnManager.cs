14be57f9a0a8c5af2b1d3a1ef0de185ba6043def
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-02-26 18:09:42 +0900

    デバッグ用に配置した場所に飛べるSpawnManagerを追加

---
+++ Assets/Scripts/System/SpawnManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オブジェクトを再生成する機能を持つクラス
+/// </summary>
+public class SpawnManager : MonoBehaviour
+{
+    /// <summary> リスポーンする場所 </summary>
+    [SerializeField]
+    Transform[] m_spawnPoints = default;
+
+    /// <summary> PlayerのTransform </summary>
+    Transform m_playerTrans = default;
+
+    [SerializeField]
+    bool m_debugMode = false;
+
+    void Start()
+    {
+        m_playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (!m_debugMode)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            m_playerTrans.position = m_spawnPoints[0].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            m_playerTrans.position = m_spawnPoints[1].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            m_playerTrans.position = m_spawnPoints[2].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            m_playerTrans.position = m_spawnPoints[3].position;
+        }
+    }
+}
