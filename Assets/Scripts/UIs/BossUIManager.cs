de301b8dfd009f76655fdcdb8144234f38b9dea0
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-03-07 00:34:37 +0900

    ボス戦時のUIの機能を持つScriptを作成

---
+++ Assets/Scripts/UIs/BossUIManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+/// <summary>
+/// ボスのUIを管理するクラス
+/// </summary>
+public class BossUIManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_bossUIPanel = default;
+
+    [SerializeField]
+    Image m_fillArea = default;
+
+    [SerializeField]
+    Image m_delayFillArea = default;
+
+    [SerializeField]
+    TextMeshProUGUI m_bossName = default;
+
+    string m_currentBossName;
+    int m_currentBossMaxHp;
+    int m_currentBossHp;
+    
+
+    public static BossUIManager Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        OffUI();
+        EventManager.ListenEvents(Events.BossBattleStart, OnUI);
+        EventManager.ListenEvents(Events.BossBattleEnd, OffUI);
+    }
+
+    void OnUI()
+    {
+        StartCoroutine(SetUp());
+    }
+
+    void OffUI()
+    {
+        m_bossUIPanel.SetActive(false);
+    }
+
+    void StartBossBattle(string bossName, int bossMaxHP)
+    {
+        m_bossName.text = bossName;
+        m_fillArea.fillAmount = bossMaxHP/bossMaxHP;
+    }
+
+    /// <summary>
+    /// ボスがダメージを受けた時の処理
+    /// </summary>
+    public void DamageHandle(int damageValue)
+    {
+        
+        m_currentBossHp -= damageValue;
+        m_fillArea.DOFillAmount((float)m_currentBossHp / m_currentBossMaxHp, 0.2f)
+                  .OnComplete(() => 
+                  {
+                      m_delayFillArea.DOFillAmount((float)m_currentBossHp / m_currentBossMaxHp, 1f);
+                  });
+        Debug.Log($"現在のHP:{m_currentBossHp},最大HP{m_currentBossMaxHp}, 残り残量：{((float)m_currentBossHp /m_currentBossMaxHp)*100}%");
+    }
+
+    IEnumerator SetUp()
+    {
+        m_bossUIPanel.SetActive(true);
+
+        yield return null;
+
+        m_currentBossName = GameManager.Instance.CurrentBossData.enemyName;
+        m_currentBossMaxHp = GameManager.Instance.CurrentBossData.maxHp;
+        m_currentBossHp = m_currentBossMaxHp;
+
+        StartBossBattle(m_currentBossName, m_currentBossMaxHp);
+    }
+}
