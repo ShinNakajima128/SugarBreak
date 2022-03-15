912891b75bda9efe0e5866c462a3fec391f1c85a
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-03-15 22:32:46 +0900

    タイトル画面のテキストのアニメーションを修正

--- Assets/Scripts/Managers/TextAnimation.cs
+++ Assets/Scripts/Managers/TextAnimation.cs
@@ -11,6 +11,7 @@ public class TextAnimation : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI m_startTmp = default;
     Sequence seq;
+    bool m_isPlaying = false;
 
     private void Awake()
     {
@@ -20,24 +21,56 @@ public class TextAnimation : MonoBehaviour
     void OnEnable()
     {
         OnAnim();
+        m_isPlaying = true;
+    }
+
+    void OnDisable()
+    {
+        m_isPlaying = false;
     }
 
     public void OnAnim()
     {
-        m_startTmp.DOFade(0, 0);
-        DOTweenTMPAnimator tmproAnimator = new DOTweenTMPAnimator(m_startTmp);
+        StartCoroutine(LoopAnimation());
+        //m_startTmp.DOFade(0, 0);
+        //DOTweenTMPAnimator tmproAnimator = new DOTweenTMPAnimator(m_startTmp);
 
-        for (int i = 0; i < tmproAnimator.textInfo.characterCount; i++)
+        //for (int i = 0; i < tmproAnimator.textInfo.characterCount; i++)
+        //{
+        //    tmproAnimator.DOScaleChar(i, 0.7f, 0);
+        //    Vector3 currCharOffset = tmproAnimator.GetCharOffset(i);
+        //    seq = DOTween.Sequence();
+
+        //    seq.SetDelay(0.25f)
+        //       .Append(tmproAnimator.DOOffsetChar(i, currCharOffset + new Vector3(0, 30, 0), 0.4f).SetEase(Ease.OutFlash, 2))
+        //       .Join(tmproAnimator.DOFadeChar(i, 1, 0.4f))
+        //       .Join(tmproAnimator.DOScaleChar(i, 1, 0.4f).SetEase(Ease.OutBack))
+        //       .SetDelay(0.07f * i);
+        //}
+    }
+
+    IEnumerator LoopAnimation()
+    {
+        yield return new WaitForSeconds(0.3f);
+        
+        while (m_isPlaying)
         {
-            tmproAnimator.DOScaleChar(i, 0.7f, 0);
-            Vector3 currCharOffset = tmproAnimator.GetCharOffset(i);
-            seq = DOTween.Sequence();
+            m_startTmp.DOFade(0, 0);
+            DOTweenTMPAnimator tmproAnimator = new DOTweenTMPAnimator(m_startTmp);
 
-            seq.SetDelay(0.25f)
-               .Append(tmproAnimator.DOOffsetChar(i, currCharOffset + new Vector3(0, 30, 0), 0.4f).SetEase(Ease.OutFlash, 2))
-               .Join(tmproAnimator.DOFadeChar(i, 1, 0.4f))
-               .Join(tmproAnimator.DOScaleChar(i, 1, 0.4f).SetEase(Ease.OutBack))
-               .SetDelay(0.07f * i);
+            for (int i = 0; i < tmproAnimator.textInfo.characterCount; i++)
+            {
+                tmproAnimator.DOScaleChar(i, 0.7f, 0);
+                Vector3 currCharOffset = tmproAnimator.GetCharOffset(i);
+                seq = DOTween.Sequence();
+
+                seq.SetDelay(0.25f)
+                   .Append(tmproAnimator.DOOffsetChar(i, currCharOffset + new Vector3(0, 30, 0), 0.4f).SetEase(Ease.OutFlash, 2))
+                   .Join(tmproAnimator.DOFadeChar(i, 1, 0.4f))
+                   .Join(tmproAnimator.DOScaleChar(i, 1, 0.4f).SetEase(Ease.OutBack))
+                   .SetDelay(0.07f * i);
+            }
+            yield return new WaitForSeconds(3.0f);
         }
     }
 
