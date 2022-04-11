631f6bd0bfe7ba070de0082c1c07001980121966
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-04-11 16:41:20 +0900

    コード修正

--- Assets/Scripts/Managers/TextAnimation.cs
+++ Assets/Scripts/Managers/TextAnimation.cs
@@ -6,12 +6,12 @@ using TMPro;
 
 public class TextAnimation : MonoBehaviour
 {
-    public static TextAnimation Instance { get; private set; }
-
     [SerializeField]
     TextMeshProUGUI m_startTmp = default;
     Sequence seq;
     bool m_isPlaying = false;
+
+    public static TextAnimation Instance { get; private set; }
 
     private void Awake()
     {
@@ -29,26 +29,18 @@ public class TextAnimation : MonoBehaviour
         m_isPlaying = false;
     }
 
+    /// <summary>
+    /// アニメーションを開始する
+    /// </summary>
     public void OnAnim()
     {
         StartCoroutine(LoopAnimation());
-        //m_startTmp.DOFade(0, 0);
-        //DOTweenTMPAnimator tmproAnimator = new DOTweenTMPAnimator(m_startTmp);
-
-        //for (int i = 0; i < tmproAnimator.textInfo.characterCount; i++)
-        //{
-        //    tmproAnimator.DOScaleChar(i, 0.7f, 0);
-        //    Vector3 currCharOffset = tmproAnimator.GetCharOffset(i);
-        //    seq = DOTween.Sequence();
-
-        //    seq.SetDelay(0.25f)
-        //       .Append(tmproAnimator.DOOffsetChar(i, currCharOffset + new Vector3(0, 30, 0), 0.4f).SetEase(Ease.OutFlash, 2))
-        //       .Join(tmproAnimator.DOFadeChar(i, 1, 0.4f))
-        //       .Join(tmproAnimator.DOScaleChar(i, 1, 0.4f).SetEase(Ease.OutBack))
-        //       .SetDelay(0.07f * i);
-        //}
     }
 
+    /// <summary>
+    /// ループアニメーション
+    /// </summary>
+    /// <returns></returns>
     IEnumerator LoopAnimation()
     {
         yield return new WaitForSeconds(0.3f);
@@ -74,6 +66,9 @@ public class TextAnimation : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// アニメーションを終了する
+    /// </summary>
     public void FinishAnim()
     {
         if (seq != null) seq.Kill();
