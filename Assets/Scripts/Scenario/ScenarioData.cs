48f909f1bdb24784b8e633cdab44b5152b20abfc
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-04-23 18:31:37 +0900

    シナリオ再生用のScriptを作成

---
+++ Assets/Scripts/Scenario/ScenarioData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create ScenarioData")]
+public class ScenarioData : ScriptableObject
+{
+    [SerializeField]
+    DialogData[] m_dialogData = default;
+
+    public DialogData[] DialogData => m_dialogData; 
+}
+
+[Serializable]
+public class DialogData
+{
+    public int Id;
+    public string[] Messages;
+}
---
+++ Assets/Scripts/Scenario/ScenarioPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScenarioPlayer : MonoBehaviour
+{
+    [Tooltip("シナリオのデータ")]
+    [SerializeField]
+    ScenarioData m_scenarioData = default;
+
+    [Tooltip("テキストの再生速度")]
+    [SerializeField]
+    float m_textSpeed = 2.0f;
+
+    [Tooltip("次の文を表示するまでの時間")]
+    [SerializeField]
+    float m_flowTime = 2.0f;
+
+    [Tooltip("Scene開始時にシナリオを再生するかどうか")]
+    [SerializeField]
+    bool m_playOnAwake = false;
+
+    [Tooltip("シナリオを表示するText")]
+    [SerializeField]
+    TextMeshProUGUI m_scenarioText = default;
+
+    /// <summary> 現在表示中のTextの指数 </summary>
+    int _currentDialogIndex = 0;
+
+    public static ScenarioPlayer Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        if (m_playOnAwake)
+        {
+            PlayScenario(m_scenarioData);
+        }
+    }
+
+    IEnumerator PlayScenario(ScenarioData data)
+    {
+        while (_currentDialogIndex < data.DialogData.Length)
+        {
+            m_scenarioText.text = "";
+
+            yield return new WaitForSeconds(m_flowTime);
+            _currentDialogIndex++;
+            BackgroundController.Instance.OnNextBackground();
+        }
+    }
+}
