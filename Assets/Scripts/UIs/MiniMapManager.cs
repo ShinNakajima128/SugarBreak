8cd9717a5a4ddb044dbc98ae53d17d9113e40c93
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-06-13 19:51:53 +0900

    ミニマップ作成、マップアイコンを表示するように追加

--- Assets/Scripts/UIs/MiniMapManager.cs
+++ Assets/Scripts/UIs/MiniMapManager.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 
 public class MiniMapManager : MonoBehaviour
 {
-    
+    [SerializeField]
+    float m_cameraRange = 20;
+    [SerializeField]
+    Transform m_miniMapCamera = default;
+
+    Transform _playerTrans;
 
     void Start()
     {
-        
+        _playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        //m_miniMapCamera.SetParent(_playerTrans);
+        //m_miniMapCamera.localPosition = new Vector3(0, 20, 0);
     }
 
+    void FixedUpdate()
+    {
+        m_miniMapCamera.position = new Vector3(_playerTrans.position.x,  m_cameraRange, _playerTrans.position.z); 
+    }
 }
