b65fe82769be14d53528218dfe1e8877375393d3
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-06-25 19:21:35 +0900

    武器メニュー画面でキャラクターを回転させる機能を作成

---
+++ Assets/Scripts/UIs/Base/UICharacterController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 回転方向の種類
+/// </summary>
+public enum RotateType
+{
+    Left,
+    Right
+}
+
+/// <summary>
+/// 武器メニュー画面のキャラクター
+/// </summary>
+public class UICharacterController : MonoBehaviour
+{
+    [Tooltip("モデルの回転角度")]
+    [SerializeField]
+    float m_rotateAngle = 30f;
+
+    Transform _characterTrans;
+    bool _init = false;
+
+    void OnEnable()
+    {
+        if (_init)
+        {
+            WeaponMenuManager.Instance.OnRotateAction += CharacterRotate;
+        }
+    }
+
+    void OnDisable()
+    {
+        WeaponMenuManager.Instance.OnRotateAction -= CharacterRotate;
+    }
+
+    void Start()
+    {
+        _characterTrans = GetComponent<Transform>();
+
+        if (!_init)
+        {
+            WeaponMenuManager.Instance.OnRotateAction += CharacterRotate;
+            _init = true;
+        }
+    }
+
+    void CharacterRotate(RotateType type)
+    {
+        switch (type)
+        {
+            case RotateType.Left:
+                _characterTrans.Rotate(new Vector3(0, m_rotateAngle, 0) * Time.deltaTime);
+                break;
+            case RotateType.Right:
+                _characterTrans.Rotate(new Vector3(0, -m_rotateAngle, 0) * Time.deltaTime);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/UIs/Base/WeaponMenuManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// 武器メニュー画面の機能を管理するクラス
+/// </summary>
+public class WeaponMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    PlayerData m_data = default;
+
+    [Tooltip("拠点のUIを管理するクラス")]
+    [SerializeField]
+    BaseUI m_baseUI = default;
+
+    [Tooltip("金平糖の数を表示するText")]
+    [SerializeField]
+    TextMeshProUGUI m_sugarPlumText = default;
+
+    /// <summary> 現在の金平糖の数 </summary>
+    int _currentSugarPlumNum = 0;
+
+    /// <summary> モデルの回転処理をまとめたAction </summary>
+    public Action<RotateType> OnRotateAction = default;
+
+    public static WeaponMenuManager Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        _currentSugarPlumNum = m_data.TotalKonpeitou;
+        m_sugarPlumText.text = _currentSugarPlumNum.ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            OnRotateAction(RotateType.Left); //左回転
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            OnRotateAction(RotateType.Right); //右回転
+        }
+    }
+
+    /// <summary>
+    /// 装備画面に戻る
+    /// </summary>
+    public void BackEquipmentPanel()
+    {
+        m_baseUI.OnEquipment();
+    }
+}
