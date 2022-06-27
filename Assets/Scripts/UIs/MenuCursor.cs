10a955f82b1cdfd0c62935a6b458f1708192401c
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-06-27 20:32:13 +0900

    メニューのカーソルの機能を追加

---
+++ Assets/Scripts/UIs/MenuCursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace SugarBreak
+{
+    /// <summary>
+    /// メニューのカーソルの機能を持つクラス
+    /// </summary>
+    public class MenuCursor : MonoBehaviour
+    {
+        public static MenuCursor Instance { get; private set; }
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// カーソルを移動する
+        /// </summary>
+        /// <param name="pos"> 移動先 </param>
+        public static void CursorMove(Vector2 pos)
+        {
+            Instance.transform.DOMove(pos, 0.25f);
+        }
+    }
+}
