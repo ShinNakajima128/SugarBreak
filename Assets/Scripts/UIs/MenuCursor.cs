926a15baf96339c0aad07e7c67f33722c93f671f
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-06-28 17:18:44 +0900

    メニューカーソルの機能を追加

--- Assets/Scripts/UIs/MenuCursor.cs
+++ Assets/Scripts/UIs/MenuCursor.cs
@@ -11,11 +11,23 @@ namespace SugarBreak
     /// </summary>
     public class MenuCursor : MonoBehaviour
     {
+        [Tooltip("カーソルの移動速度")]
+        [SerializeField]
+        float _moveSpeed = 0.1f;
+
+        Image _cursorImage;
         public static MenuCursor Instance { get; private set; }
+        public bool IsActive { get; set; } = false;
 
         void Awake()
         {
             Instance = this;
+        }
+
+        void Start()
+        {
+            _cursorImage = GetComponent<Image>();
+            OffCursor();
         }
 
         /// <summary>
@@ -24,7 +36,26 @@ namespace SugarBreak
         /// <param name="pos"> 移動先 </param>
         public static void CursorMove(Vector2 pos)
         {
-            Instance.transform.DOMove(pos, 0.25f);
+            if (!Instance.IsActive)
+            {
+                OnCursor();
+            }
+            Instance.transform.DOMove(pos, Instance._moveSpeed);
+        }
+
+        /// <summary>
+        /// カーソル表示をONにする
+        /// </summary>
+        public static void OnCursor()
+        {
+            Instance._cursorImage.enabled = true;
+        }
+        /// <summary>
+        /// カーソル表示をOFFにする
+        /// </summary>
+        public static void OffCursor()
+        {
+            Instance._cursorImage.enabled = false;
         }
     }
 }
