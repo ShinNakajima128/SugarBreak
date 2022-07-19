479e86542e6d4b08f1f01901f3f62edce727cccd
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-07-19 23:04:42 +0900

    ゲームパッドのスティックでカーソルを操作するScriptを作成

---
+++ Assets/Scripts/UIs/CursolController.cs
@@ -0,0 +1,20 @@
+﻿//参考サイト:https://qiita.com/kirurobo/items/fb6b39a6097338f02eb4
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UniRx;
+using UniRx.Triggers;
+
+public class CursolController : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    public static extern bool SetCursorPos(int x, int y);
+
+
+    void Start()
+    {
+        
+    }
+}
