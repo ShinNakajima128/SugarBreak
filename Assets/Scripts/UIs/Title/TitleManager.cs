458d49a1c05b9b6258606680f6cb12075b08f44a
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-07-20 21:54:54 +0900

    タイトル画面の機能を管理するTitleManagerを作成

---
+++ Assets/Scripts/UIs/Title/TitleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public enum TitleMode
+{
+    /// <summary> 通常のタイトル画面 </summary>
+    Normal,
+    /// <summary> TGS用のタイトル画面 </summary>
+    TGS
+}
+public class TitleManager : MonoBehaviour
+{
+    [Tooltip("タイトルのモード")]
+    [SerializeField]
+    TitleMode _titleMode = default;
+
+    [SerializeField]
+    TitleMenuController _titleMenuCtrl = default;
+
+    void Start()
+    {
+        AudioManager.PlayBGM(BGMType.Title);
+    }
+}
