3be6995966d6c343425d2b1f5fb11f90fc4f82f4
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-08-05 16:29:22 +0900

    タイトル画面で最初にボタンを押した時の処理を作成

--- Assets/Scripts/UIs/Title/TitleManager.cs
+++ Assets/Scripts/UIs/Title/TitleManager.cs
@@ -23,7 +23,44 @@ public class TitleManager : MonoBehaviour
     void Start()
     {
         AudioManager.PlayBGM(BGMType.Title);
-        
+
+        _titleMenuCtrl.OnMenuPanel(MenuType.Start);
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.anyKeyDown && _titleMenuCtrl.CurrentMenuType == MenuType.Start)
+            .Subscribe(_ => 
+            {
+                OnMainMenuPanel();
+                AudioManager.PlaySE(SEType.UI_Select);
+            });
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKeyDown(KeyCode.Escape) && _titleMenuCtrl.CurrentMenuType != MenuType.Start)
+            .Subscribe(_ =>
+            {
+                _titleMenuCtrl.OnMenuPanel(MenuType.Start);
+                AudioManager.PlaySE(SEType.UI_Cancel);
+            });
+    }
+
+    #region button method
+    /// <summary>
+    /// ゲームを終了する
+    /// </summary>
+    public void OnGameExit()
+    {
+        LoadSceneManager.Instance.QuitGame();
+    }
+    /// <summary>
+    /// ゲーム終了をキャンセルし、メインメニューへ戻る
+    /// </summary>
+    public void OnCancel()
+    {
+        _titleMenuCtrl.OnMenuPanel(MenuType.MainMenu);
+    }
+    #endregion
+    void OnMainMenuPanel()
+    {
         switch (_titleMode)
         {
             case TitleMode.Normal:
