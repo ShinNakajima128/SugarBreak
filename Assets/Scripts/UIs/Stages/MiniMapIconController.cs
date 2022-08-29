e78aab166b5dd032cf6e765684409e8a8218316e
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-08-29 15:04:33 +0900

    ミニマップの機能にカメラの向きとプレイヤーの向きを各アイコンへ同期する処理を作成

---
+++ Assets/Scripts/UIs/Stages/MiniMapIconController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMapIconController : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform _playerIcon = default;
+
+    [SerializeField]
+    RectTransform _cameraDirIcon = default;
+
+    [SerializeField]
+    Transform _playerObj = default;
+
+    [SerializeField]
+    Transform _camera = default;
+
+    void Update()
+    {
+        Vector3 playerRot = new Vector3(0, 0, _playerObj.rotation.y);
+        _playerIcon.localRotation = Quaternion.Euler(0, 0, -_playerObj.localEulerAngles.y);
+        Vector3 cameraRot = new Vector3(0, 0, Camera.main.transform.rotation.y);
+        _cameraDirIcon.localRotation = Quaternion.Euler(0, 0, -_camera.localEulerAngles.y);
+    }
+}
