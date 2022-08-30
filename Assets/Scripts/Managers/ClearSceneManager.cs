0ac44d86b282c12eed69836cfeb3ef68993f604f
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-08-30 18:08:30 +0900

    ボスを倒した後にクリアシーンへ遷移する処理を作成

---
+++ Assets/Scripts/Managers/ClearSceneManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClearSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    Fade _fade = default;
+
+    [SerializeField]
+    Image _charaImage = default;
+
+    [SerializeField]
+    Sprite _winkImage = default;
+
+    Coroutine _fadeCol;
+
+    void Start()
+    {
+        StartCoroutine(ClearDirection());
+    }
+
+    IEnumerator ClearDirection()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        yield return new WaitUntil(() => Input.anyKeyDown);
+
+        _fadeCol = _fade.FadeIn(3.0f);
+        yield return new WaitForSeconds(0.8f);
+        
+        StopCoroutine(_fadeCol);
+        _fadeCol = null;
+
+        _charaImage.sprite = _winkImage;
+
+        yield return new WaitForSeconds(1.5f);
+
+        _fadeCol = _fade.FadeIn(0.5f, () => 
+        {
+            LoadSceneManager.Instance.LoadTitle();
+        });
+    }
+}
