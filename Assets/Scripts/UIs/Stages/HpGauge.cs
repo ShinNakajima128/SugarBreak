df4778bb19f15ce50633387b43b6ac96551ea261
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-09-09 16:58:49 +0900

    HPのアニメーションを作成

--- Assets/Scripts/UIs/Stages/HpGauge.cs
+++ Assets/Scripts/UIs/Stages/HpGauge.cs
@@ -1,62 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Linq;
 
 public class HpGauge : MonoBehaviour
 {
-    [Header("1番目の偶数体力")]
-    [SerializeField] 
-    GameObject m_evenHpObj1 = default;
-
-    [Header("2番目の偶数体力")]
+    [Header("偶数体力")]
     [SerializeField]
-    GameObject m_evenHpObj2 = default;
+    Image[] _evenHpObj = default;
 
-    [Header("3番目の偶数体力")]
+    [Header("奇数体力")]
     [SerializeField]
-    GameObject m_evenHpObj3 = default;
-
-    [Header("1番目の奇数体力")]
-    [SerializeField] 
-    GameObject m_addHpObj1 = default;
-
-    [Header("2番目の奇数体力")]
-    [SerializeField]
-    GameObject m_addHpObj2 = default;
-
-    [Header("3番目の奇数体力")]
-    [SerializeField]
-    GameObject m_addHpObj3 = default;
+    Image[] _addHpObj = default;
 
     /// <summary> プレイヤーのデータ </summary>
     [SerializeField]
     PlayerData playerData = default;
 
+    [SerializeField]
+    Transform _hpListTrans = default;
+
+    List<Image> _currentHpList = new List<Image>();
+    Vector3 _originListPosition;
+
+    void Start()
+    {
+        _originListPosition = _hpListTrans.position;
+        Debug.Log(_originListPosition);
+    }
+
     /// <summary>
     /// HPの数値に応じて体力オブジェクトをセットする
     /// </summary>
     /// <param name="hp"> HPの値 </param>
-    public void SetHpGauge(int hp)
+    public void SetHpGauge(int hp, bool isDamaged = false)
     {
         ///　体力を一旦全削除
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < _hpListTrans.childCount; i++)
         {
-            Destroy(transform.GetChild(i).gameObject);
+            Destroy(_hpListTrans.GetChild(i).gameObject);
+            
         }
-        ///　現在の体力数分のライフゲージを作成
-        //for (int i = 1; i <= hp; i++)
-        //{
-        //    if (i == hp && i % 2 == 1)
-        //    {
-        //        Instantiate(m_addHpObj1, transform);
-
-        //    }
-        //    else if (i % 2 == 0)
-        //    {
-        //        Instantiate(m_evenHpObj1, transform);
-
-        //    }
-        //}
+        _currentHpList.Clear();
 
         for (int i = 1; i <= hp; i++)
         {
@@ -64,46 +51,67 @@ public class HpGauge : MonoBehaviour
             {
                 if (i == 7)
                 {
-                    Instantiate(m_addHpObj1, transform);
+                    _currentHpList.Add(Instantiate(_addHpObj[0], _hpListTrans));
                 }
                 else if (i == 5)
                 {
-                    Instantiate(m_addHpObj3, transform);
+                    _currentHpList.Add(Instantiate(_addHpObj[2], _hpListTrans));
 
                 }
                 else if (i == 3)
                 {
-                    Instantiate(m_addHpObj2, transform);
+                    _currentHpList.Add(Instantiate(_addHpObj[1], _hpListTrans));
 
                 }
                 else if (i == 1)
                 {
-                    Instantiate(m_addHpObj1, transform);
+                    _currentHpList.Add(Instantiate(_addHpObj[0], _hpListTrans));
                 }
-
 
             }
             else if (i % 2 == 0)
             {
                 if (i == 8)
                 {
-                    Instantiate(m_evenHpObj1, transform);
+                    _currentHpList.Add(Instantiate(_evenHpObj[0], _hpListTrans));
                 }
                 else if (i == 6)
                 {
-                    Instantiate(m_evenHpObj3, transform);
+                    _currentHpList.Add(Instantiate(_evenHpObj[2], _hpListTrans));
 
                 }
                 else if (i == 4)
                 {
-                    Instantiate(m_evenHpObj2, transform);
+                    _currentHpList.Add(Instantiate(_evenHpObj[1], _hpListTrans));
 
                 }
                 else if (i == 2)
                 {
-                    Instantiate(m_evenHpObj1, transform);
+                    _currentHpList.Add(Instantiate(_evenHpObj[0], _hpListTrans));
                 }
             }
+        }
+
+        if (hp > 2)
+        {
+            _currentHpList[_currentHpList.Count - 1].gameObject.transform.DOScale(1.2f, 0.5f)
+                                                                         .SetLoops(-1, LoopType.Yoyo);
+        }
+        else if (hp <= 2 && hp > 0)
+        {
+            _currentHpList[_currentHpList.Count - 1].DOColor(Color.red, 0.5f)
+                                                    .SetLoops(-1, LoopType.Yoyo);
+            _currentHpList[_currentHpList.Count - 1].gameObject.transform.DOScale(1.2f, 0.5f)
+                                                                         .SetLoops(-1, LoopType.Yoyo);
+        }
+        
+        if (isDamaged)
+        {
+            _hpListTrans.DOShakePosition(0.25f, 10, 20)
+                        .OnComplete(() => 
+                        {
+                            _hpListTrans.position = _originListPosition;
+                        });
         }
     }
 }
