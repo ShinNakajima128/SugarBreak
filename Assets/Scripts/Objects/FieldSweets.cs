a10d0f3ecb8a26ee03fee501c1efcbd2e0fbfd30
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-09-13 03:43:10 +0900

    フィールドのお菓子オブジェクトの機能を修正

--- Assets/Scripts/Objects/FieldSweets.cs
+++ Assets/Scripts/Objects/FieldSweets.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using ObjectOutline;
+using UniRx;
 
 /// <summary>
 /// ステージに存在するオブジェクトのデータを持つクラス
@@ -10,9 +10,75 @@ public class FieldSweets : MonoBehaviour
     [SerializeField]
     FieldSweetsData m_sweetsData = default;
 
+    [SerializeField]
+    OutlineActivator _outlineAct = default;
+
+    Outline _outline;
+    ReactiveProperty<bool> _isEnable = new ReactiveProperty<bool>(false);
+
+    void Start()
+    {
+        if (TryGetComponent<Outline>(out _outline))
+        {
+            //Debug.Log("Outlineコンポーネント取得");
+        };
+
+        if (_outlineAct != null) 
+        {
+            _outlineAct.OnReactiveAction += OnOutline;
+            
+            _outlineAct.OffReactiveAction += OffOutline;
+        };
+        EventManager.ListenEvents(Events.OnSweetsOutline, Activate);
+        EventManager.ListenEvents(Events.OffSweetsOutline, Deactivate);
+
+        _isEnable.Subscribe((x) =>
+        {
+            if (x)
+            {
+                OnOutline();
+            }
+            else
+            {
+                OffOutline();
+            }
+        });
+    }
+    
     public FieldSweetsSize SweetsType => m_sweetsData.SweetsSize;
     public Vector3 ColliderSize => m_sweetsData.ColliderSize;
     public int AttackPower => m_sweetsData.AttackPower;
     public int KonpeitouNum => m_sweetsData.KonpeitouNum;
     public int EnduranceCount => m_sweetsData.EnduranceCount;
+    public Outline Outline => _outline;
+
+    void Activate()
+    {
+        _isEnable.Value = true;
+    }
+
+    void Deactivate()
+    {
+        _isEnable.Value = false;
+    }
+
+    void OnOutline()
+    {
+        if (_outlineAct == null)
+        {
+            return;
+        }
+
+        if (_outline != null && _isEnable.Value && _outlineAct.IsInArea)
+        {
+            _outline.enabled = true;
+        }
+    }
+    void OffOutline()
+    {
+        if (_outline != null)
+        {
+            _outline.enabled = false;
+        }
+    }
 }
