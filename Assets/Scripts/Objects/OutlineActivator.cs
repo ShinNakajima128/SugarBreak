2556488a4f6744a19acebc8cebc376ad0c29b013
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-09-13 03:36:37 +0900

    フォークで刺せるオブジェクトにアウトラインを表示する機能を追加

---
+++ Assets/Scripts/Objects/OutlineActivator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class OutlineActivator : MonoBehaviour
+{
+    ReactiveProperty<bool> _isInArea = new ReactiveProperty<bool>(false);
+    public bool IsInArea => _isInArea.Value;
+    public Action OnReactiveAction;
+    public Action OffReactiveAction;
+
+    void Start()
+    {
+        _isInArea.Subscribe((x) =>
+        {
+            if (x)
+            {
+                OnReactiveAction?.Invoke();
+                //Debug.Log("リアクティブ:True");
+            }
+            else
+            {
+                OffReactiveAction?.Invoke();
+                //Debug.Log("リアクティブ:false");
+            }
+        });
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == "Player")
+        {
+            //Debug.Log("プレイヤーヒット");
+            _isInArea.Value = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Debug.Log("プレイヤーが離れた");
+            _isInArea.Value = false;
+        }
+    }
+}
